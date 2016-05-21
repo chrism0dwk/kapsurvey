af37d1e996cf30142edac82a8ede4d37dd36ed7a
Chris Jewell <chrism0dwk@gmail.com>
2016-05-21 11:29:54 +0100

    First draft of KAP survey.

--- js/base.js
+++ js/base.js
@@ -1,58 +1,146 @@
+// Add 'show' and 'hide' events
+(function ($) {
+      $.each(['show', 'hide'], function (i, ev) {
+        var el = $.fn[ev];
+        $.fn[ev] = function () {
+          this.trigger(ev);
+          return el.apply(this, arguments);
+        };
+      });
+})(jQuery);
+
 $(function() {
-    var i = Number(localStorage.getItem('todo-counter')) + 1,
+    var i = Number(localStorage.getItem('item-counter')) + 1,
 	j = 0,
 	k,
+	$name = $('#qu-form-name'),
+	$role = $('#qu-form-role'),
 	$nextpagebutton = $('#next-button'),
 	$questiontext = $('#question-text'),
-	$form = $('#todo-form'),
-	$nextpage = $('#form-submitter'),
-	$removeLink = $('#show-items li a'),
-	$itemList = $('#show-items'),
+	$freelist = $('#freelist-form'),
+	$removeLink = $('#show-freelist-items li a'),
+	$itemList = $('#show-freelist-items'),
 	$editable = $('.editable'),
 	$clearAll = $('#clear-all'),
-	$newTodo = $('#todo'),
+	$newItem = $('#freelist-item'),
 	order = [],
 	orderList;
 
-    // Set submit form properties
-    $nextpage.data('page','freelist');
+    $('.page').hide();
+    $('#page-1').show();
 
-    // Load todo list
-    orderList = localStorage.getItem('todo-orders');
-    
-    orderList = orderList ? orderList.split(',') : [];
-    
-    for( j = 0, k = orderList.length; j < k; j++) {
-	$itemList.append(
-            "<li id='" + orderList[j] + "'>"
-		+ "<span class='editable'>" 
-		+ localStorage.getItem(orderList[j]) 
-		+ "</span> <a href='#'>X</a></li>"
-	);
+    var dataUploaded = false;
+
+    // Send JSON functionality
+    sendJSON = function(d) {
+	console.log(d);
+	$.post("http://logs-01.loggly.com/inputs/8b49d6fa-af6c-441a-9842-daa7316bddd3/tag/http/",
+	       JSON.stringify(d),
+	       function() { console.log("Data uploaded");
+			    $('.uploader').trigger('/dataUploaded/');
+			  });
+	
     }
 
-    // Risks --> ranking
-    $nextpage.submit(function(e) {
-    	e.preventDefault();
-	
-    	$itemList.sortable({
-    	    disabled: false,
-    	});
-	$questiontext.text("Now please rank the risks, putting the risk you consider most important at the top.");
-	//document.getElementById("next-button").value = "Submit";
-	$nextpagebutton.val("Submit");
-    });
+    sendConfirm = function() {
+	var rv = confirm("These data will be stored for analysis.  Continue?");
+	return rv;
+    }
+
+    packRisks = function(orderList) {
+	var $todoItemLi = $('#rank-freelist-items li')
+	var d = {};
+	$todoItemLi.each(function(index) {
+	    d[index] = $( this ).text().split(" ")[0];
+	});
+	return d;
+    }
+
+    sendFormData = function() {
+	var d = { 'id' : uuid.v4(),
+    		  'name' : $name.val(),
+    		  'role' : $role.val(),
+    		  'risks' : packRisks(orderList),
+    		};
+    	if(sendConfirm()) {
+    	    sendJSON(d);
+    	}
+    }
+
+    var spinner = new Spinner();
     
-    // Add todo
-    $form.submit(function(e) {
+    // Page progression
+    $('#page1-submit').submit(function(e) {
+	e.preventDefault();
+	if($.trim($name.val()) == '')
+	    alert('Please enter your name');
+	else if($.trim($role.val()) == '')
+	    alert('Please enter your role');
+	else {
+	    $('#page-1').hide();
+	    $('#page-2').show();
+	}
+    });
+    $('#page2-submit').submit(function(e) {
+	e.preventDefault();
+	orderList = localStorage.getItem('item-orders');
+	if(!orderList) {
+	    alert('Please enter a risk');
+	}
+	else {
+	    $('#page-2').hide();
+	    $('#page-3').show();
+	}
+    });
+    $('#page3-submit').submit(function(e) {
+	e.preventDefault();
+	console.log("page-3 submit pressed");
+	if(sendConfirm()) {
+	    spinner.spin(document.getElementById('page3-spinner'));
+	    sendFormData();
+	}
+    });
+    $('#page-3').bind('/dataUploaded/', function(e) {
+	spinner.stop();
+	$('#page-3').hide();
+	$('#page-4').show();
+    });
+    $('#page4-submit').submit(function(e) {
+	$.publish('/clear-all/',[]);
+    });
+
+    // Page 3 populate list
+    $('#page-3').bind('show', function() {
+	orderList = localStorage.getItem('item-orders');
+	orderList = orderList ? orderList.split(',') : [];
+	$itemList = $('#rank-freelist-items');
+	for( j = 0, k = orderList.length; j < k; j++) {
+	    $itemList.append(
+		"<li class='draggable' id='" + orderList[j] + "'>"
+		    + "<span>" 
+		    + localStorage.getItem(orderList[j]) 
+		    + "</span></li>"
+	    );
+	}
+	$itemList.sortable({
+	    disabled: false,
+	});
+    });
+
+    $('#page-4').bind('show', function() {
+	$('#name').text($name.val());
+    });
+			      
+    
+    // Add item
+    $freelist.submit(function(e) {
 	e.preventDefault();
 	$.publish('/add/', []);
     });
 
-    // Remove todo
+    // Remove item
     $itemList.delegate('a', 'click', function(e) {
 	var $this = $(this);
-        
 	e.preventDefault();
 	$.publish('/remove/', [$this]);
     });
@@ -95,32 +183,32 @@ $(function() {
     
     // Subscribes
     $.subscribe('/add/', function() {
-	if ($newTodo.val() !== "") {
+	if ($newItem.val() !== "") {
 	    // Take the value of the input field and save it to localStorage
 	    localStorage.setItem( 
-		"todo-" + i, $newTodo.val() 
+		"item-" + i, $newItem.val() 
 	    );
             
 	    // Set the to-do max counter so on page refresh it keeps going up instead of reset
-	    localStorage.setItem('todo-counter', i);
+	    localStorage.setItem('item-counter', i);
             
 	    // Append a new list item with the value of the new todo list
 	    $itemList.append(
-                "<li id='todo-" + i + "'>"
+                "<li id='item-" + i + "'>"
                     + "<span class='editable'>"
-                    + localStorage.getItem("todo-" + i) 
+                    + localStorage.getItem("item-" + i) 
                     + " </span><a href='#'>x</a></li>"
 	    );
 
 	    $.publish('/regenerate-list/', []);
 
 	    // Hide the new list, then fade it in for effects
-	    $("#todo-" + i)
+	    $("#item-" + i)
 		.css('display', 'none')
 		.fadeIn();
             
 	    // Empty the input field
-	    $newTodo.val("");
+	    $newItem.val("");
             
 	    i++;
 	}
@@ -143,7 +231,7 @@ $(function() {
     });
     
     $.subscribe('/regenerate-list/', function() {
-	var $todoItemLi = $('#show-items li');
+	var $todoItemLi = $('#show-freelist-items li');
 	// Empty the order array
 	order.length = 0;
         
@@ -155,16 +243,20 @@ $(function() {
         
 	// Convert the array into string and save to localStorage
 	localStorage.setItem(
-	    'todo-orders', order.join(',')
+	    'item-orders', order.join(',')
 	);
     });
     
     $.subscribe('/clear-all/', function() {
-	var $todoListLi = $('#show-items li');
+	var $todoListLi = $('#show-freelist-items li');
         
 	order.length = 0;
 	localStorage.clear();
 	$todoListLi.remove();
     });
+
+
+    // Clear storage to begin
+    $.publish('/clear-all/',[]);
 });
 
